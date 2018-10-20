12cda9e2506b20869c21ebef9613f97e066ac954
asif <asif.mehedi@live.com>
2018-10-20 19:19:27 -0400

    First commit

---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<template lang="pug">
+  .uk-container
+    .uk-card.uk-card-default.uk-card-body
+      .uk-h2.uk-padding-large This is a heading
+      p Sunt dolor magna minim et duis et laboris esse ad ad cillum dolore consectetur. Eiusmod minim minim ex veniam incididunt. Velit sit magna proident do. Proident ipsum sunt adipisicing ullamco et. Anim fugiat pariatur ex esse laboris esse est sint.
+      button.uk-button.uk-button-default.uk-button-primary.uk-margin-xlarge button
+      p.uk-alert.uk-alert-primary Sint tempor incididunt anim quis consectetur laborum fugiat. Id adipisicing duis incididunt labore anim laborum mollit incididunt sint. Ipsum esse officia pariatur irure sunt sunt amet excepteur magna ex consectetur irure pariatur laboris. Enim qui ea excepteur adipisicing laboris. Nulla incididunt eu magna minim dolor consectetur.
+
+      p Laborum aliqua et dolore velit anim do et. Esse sit magna nulla aliqua esse in Lorem ea. Ea excepteur ut exercitation cupidatat. Sint velit laboris culpa occaecat cillum non. Non minim amet aute qui elit. Do deserunt incididunt sint aute. Lorem proident nisi enim laboris eiusmod minim in.
+</template>
