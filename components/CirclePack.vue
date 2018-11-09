8a5a4c44c230241522b2d23604dcf9c3e0600673
asif <asif.mehedi@live.com>
2018-11-08 20:56:42 -0500

    Change body font to FG

---
+++ components/CirclePack.vue
@@ -0,0 +1,35 @@
+<script>
+import * as d3 from 'd3';
+import { dataProm } from 'assets/js/fetchData';
+
+import { getCountriesProm } from 'assets/js/computeData';
+
+// getCountriesProm.then(data => {
+//   console.log(data);
+// });
+export default {
+  data() {
+    return {
+      countries: [],
+    };
+  },
+  mounted() {
+    this.getCountries();
+  },
+  methods: {
+    getCountries() {
+      getCountriesProm.then(countries => {
+        // console.log('countries', countries);
+        this.countries = countries;
+      });
+    },
+  },
+};
+</script>
+
+<template lang="pug">
+div
+  p inside circlepack
+  svg#svg
+  p countries are: {{ countries }}
+</template>
---
+++ tests/index.spec.js
@@ -0,0 +1,10 @@
+// import { mount } from '@vue/test-utils';
+// import map from '@/components/map';
+
+// describe('map.vue', () => {
+//   const wrapper = mount(map);
+//   debugger;
+//   test('should have svg element', () => {
+//     expect(wrapper.contains('svg')).toBe(true);
+//   });
+// });
