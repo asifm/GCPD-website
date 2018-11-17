4a0dc8c0ea02f8c20b89f787f5aafdb8b0adc408
asif <asif.mehedi@live.com>
2018-11-16 20:21:30 -0500

    Add layouts and pages for testing

---
+++ pages/iframetest.vue
@@ -0,0 +1,19 @@
+
+<script>
+// import components
+import ExploreMap from '@/components/ExploreMap';
+
+export default {
+  layout: 'ContentOnly',
+  components: { ExploreMap },
+};
+</script>
+
+<template lang="pug">
+  
+  //- Map
+  .uk-section
+    explore-map
+
+ 
+</template>
---
+++ pages/test.vue
@@ -0,0 +1,19 @@
+<script>
+import MapMain from '@/components/MapMain';
+import BarGraph from '@/components/BarGraph';
+export default {
+  components: { MapMain, BarGraph },
+  data() {
+    return {};
+  },
+};
+</script>
+a
+<template lang="pug">
+div.uk-container.uk-container-large
+  p Voluptate nulla est adipisicing proident amet sunt esse sint elit. Reprehenderit labore id Lorem enim. Exercitation deserunt incididunt voluptate dolor velit. Labore aliquip eu eiusmod eiusmod esse. Mollit Lorem reprehenderit cillum voluptate. Dolore culpa nulla ea minim adipisicing.
+  p within test.vue Magna Lorem commodo culpa tempor excepteur proident aliqua excepteur laboris cillum. Quis labore laborum aliquip sit occaecat sit excepteur eiusmod minim ad fugiat qui. Nulla veniam elit do ipsum nostrud. Adipisicing nisi veniam excepteur id ad non commodo veniam voluptate et sit.
+  .uk-card.uk-card-default.uk-card-body
+  bar-graph
+  //- map-main
+</template>
