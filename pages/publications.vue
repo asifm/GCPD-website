cb5463a90226f10439d92f019e83ebf53744543d
asif <asif.mehedi@live.com>
2018-11-16 20:37:30 -0500

    Change layout and styling of publications page

--- pages/publications.vue
+++ pages/publications.vue
@@ -5,27 +5,34 @@ export default {
       publications: [
         {
           title:
-            'Testing “the Locust Hypothesis”: the Impact of Global Investors on Long-term Investment and Innovation',
+            'Are foreign investors locusts? The long-term effects of foreign institutional ownership',
+          authors: 'Jan Bena, Miguel A Ferreira, Pedro Matos, Pedro Pires',
+          date: 'October 2017',
+          publication: 'Journal of Financial Economics',
           intro:
-            'As the world economy has become increasingly globalized and complex, it’s not obvious what effects foreign investors—say, Chinese investors in an American company or American investors in a British company—have on the companies whose stock they hold. Is it possible that these foreign investors provide pools of capital that firms can access in order to produce stronger companies, which make better products and services? Do these investors from afar monitor firms—for example, keeping managers from becoming too entrenched or engaging in value-destroying activities? Or does the opposite hold true, and do global institutional investors behave more like a swarm of locusts, resulting in short-term corporate policymaking?',
+            'This paper challenges the view that foreign investors lead firms to adopt a short-term orientation and forgo long-term investment. Using a comprehensive sample of publicly listed firms in 30 countries over the period 2001–2010, we find instead that greater foreign institutional ownership fosters long-term investment in tangible, intangible, and human capital. Foreign institutional ownership also leads to significant increases in innovation output.',
           imgSrc:
-            'https://res.cloudinary.com/asifm/image/upload/h_400/v1541443135/locust-briefing-cover_lwu3n2.png',
-          downloadLink: '../assets/documents/UVA-Darden-Research-Briefing.pdf',
-          readMoreLink: ''
+            'https://ars.els-cdn.com/content/image/1-s2.0-S0304405X17X0010X-cov150h.gif',
+          downloadLink: '',
+          readMoreLink:
+            'https://www.sciencedirect.com/science/article/pii/S0304405X17301526',
         },
         {
           title:
-            'Do laborum pariatur incididunt ut. Ullamco ea laborum nulla reprehenderit.',
+            'Testing “the Locust Hypothesis”: the Impact of Global Investors on Long-term Investment and Innovation',
+          authors: 'Pedro Matos, Jan Bena, Miguel A Ferreira, Pedro Pires',
+          date: 'November 2018',
+          publication: 'UVA Darden Research Briefings',
           intro:
-            'Deserunt ipsum esse elit Lorem velit aliquip elit tempor aliquip qui minim eu dolore sint. Labore laborum magna esse aliqua laborum esse minim proident magna. Do nostrud in aliquip amet veniam sint cillum enim ea.',
+            'As the world economy has become increasingly globalized and complex, it’s not obvious what effects foreign investors—say, Chinese investors in an American company or American investors in a British company—have on the companies whose stock they hold. Is it possible that these foreign investors provide pools of capital that firms can access in order to produce stronger companies, which make better products and services? Do these investors from afar monitor firms—for example, keeping managers from becoming too entrenched or engaging in value-destroying activities? Or does the opposite hold true, and do global institutional investors behave more like a swarm of locusts, resulting in short-term corporate policymaking?',
           imgSrc:
-            'https://res.cloudinary.com/asifm/image/upload/h_400/v1541443135/locust-briefing-cover_lwu3n2.png',
+            'https://res.cloudinary.com/asifm/image/upload/h_250/v1541443135/locust-briefing-cover_lwu3n2.png',
           downloadLink: '../assets/documents/UVA-Darden-Research-Briefing.pdf',
-          readMoreLink: ''
-        }
-      ]
+          readMoreLink: '',
+        },
+      ],
     };
-  }
+  },
 };
 </script>
 1
@@ -39,15 +46,17 @@ export default {
       .uk-grid.uk-grid(uk-grid).uk-width-1-1
         div(class="uk-width-1-3@m uk-width-1-2@s" v-for="(pub, i) in publications" :key="i")
           .uk-card.uk-card-body.uk-card-default
-            //- (:class="i%2 ? 'uk-card-primary' : 'uk-card-secondary'")
-            h5.fg-orange-darken-2 {{ pub.title }}
-            h6.uk-margin-remove-top.uk-margin-medium-bottom November 2018
-          
-            button.uk-button.uk-button-default.uk-float-left.uk-margin
-              a(:href="pub.downloadLink" download) Download
-          
-            a(href="pub.downloadLink" download)
+            .uk-card-header
+              h5.fg-blue {{ pub.title }}
+              .uk-text-bold.uk-margin-remove-top <span class="fg-orange">{{ pub.date }}</span> <br> <span class="fg-blue">{{ pub.publication }}</span> 
+              p {{ pub.authors }}
+              button.uk-button.uk-button-default.uk-float-left.uk-margin(v-if="pub.downloadLink")
+                a(:href="pub.downloadLink" download) Download
+
+              button.uk-button.uk-button-default.uk-float-left.uk-margin(v-if="pub.readMoreLink")
+                a(:href="pub.readMoreLink" ) Read More
+
               img.uk-box-shadow-large(:src="pub.imgSrc")
-            p {{ pub.intro }}
+              p.uk-text-small.fg-blue {{ pub.intro }}
        
 </template>
