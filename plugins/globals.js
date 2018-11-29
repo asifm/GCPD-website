537b5550aeb5b643d8aa05d0c68863d7d55d49bb
asif <asif.mehedi@live.com>
2018-11-29 16:52:08 -0500

    Add element-ui and vuetify

--- plugins/globals.js
+++ plugins/globals.js
@@ -1,11 +1,28 @@
+// All plugins here are automatically registered with nuxt.config; because this file itself is registered there as a plugin
+
 import Vue from 'vue';
+
+// large ui libraries
 import Vuikit from 'vuikit';
+import Element from 'element-ui';
+import Vuetify from 'vuetify';
+// to use vuetify styles, add the following styles/fonts
+// <link href="https://cdn.jsdelivr.net/npm/vuetify/dist/vuetify.min.css" rel="stylesheet"></link>
+// <link href='https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons' rel="stylesheet"></link>
+
+// one or a few ui components
 import vSelect from 'vue-select';
 import vuescroll from 'vue-scroll';
 
+// :: register globally
 Vue.use(Vuikit);
 
-// ? not working, uninstall
+// ::register only required components
+// Vue.use(Vuetify);
+// Vue.use(Element);
+
+// ! not working; perhaps fixed in new nuxt versin
 Vue.use(vuescroll);
 
+// In use in the interactive page
 Vue.component('v-select', vSelect);
