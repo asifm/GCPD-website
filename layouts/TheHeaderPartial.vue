e18027b825eee7322cfa1ebdeb3e789736f6070f
asif <asif.mehedi@live.com>
2018-11-30 17:01:57 -0500

    Fix and clean up nav bar

--- layouts/TheHeaderPartial.vue
+++ layouts/TheHeaderPartial.vue
@@ -1,50 +1,19 @@
 <template lang="pug">
-  //- Above the fold
-  //- Top menu/logo bar
-  //- TODO: Hide in small screen / mobile
-  //- .bg-blue.uk-position-z-index(uk-sticky="animation:uk-animation-fade" uk-grid).uk-grid
-  .uk-section.bg-blue(uk-sticky uk-grid).uk-grid
-    
-    .uk-width-1-4.uk-padding-remove
-      img(src="https://res.cloudinary.com/asifm/image/upload/h_70/v1540605162/darden-logo-dark-new_m2cxtt.png")
-    
-    div(class="uk-width-3-4@m uk-visible@m")
-      ul.tab.uk-navbar-nav.uk-float-right
-        li(v-if="this.$route.path != '/'"): nuxt-link(to="/") Home
-        li: nuxt-link(to="/data_highlights") Data Highlights
-        li: nuxt-link(to="/interactive") Interactive
-        li: nuxt-link(to="/publications") Publications
-        li: nuxt-link(to="/download_data") Download Data
-        li: nuxt-link(to="/about_us") About Us
+  nav.uk-navbar-right(uk-navbar uk-sticky="show-on-up:true; animation:uk-animation-slide-top").uk-box-shadow-small.bg-white
+    img(src="https://res.cloudinary.com/asifm/image/upload/w_300/v1543573588/darden_rgb-02_qrpxwe.png").uk-float-left.uk-margin-left
+    .uk-navbar-right.uk-margin-right
+      ul.uk-navbar-nav
+        li.uk-button-text(v-if="this.$route.path != '/'"): nuxt-link(to="/") Home
+        li.uk-button-text
+          nuxt-link(to="/data_highlights") Data Highlights
+          .uk-navbar-dropdown
+            ul.uk-nav.uk-navbar-dropdown-nav
+              li.uk-button-text: nuxt-link(to="#") Companies
+              li.uk-button-text: nuxt-link(to="#") Industries
+              li.uk-button-text: nuxt-link(to="#") Countries
+        li.uk-button-text: nuxt-link(to="/interactive") Interactive
+        li.uk-button-text: nuxt-link(to="/publications") Publications
+        li.uk-button-text: nuxt-link(to="/download_data") Download Data
+        li.uk-button-text.my-text-medium: nuxt-link(to="/about_us") About Us
+
 </template>
-
-<script>
-export default {
-  data() {
-    // console.log(this.$route.path);
-    return {};
-  },
-};
-</script>
-
-<style lang="scss" scoped>
-.tab a {
-  font-family: Roboto;
-  font-weight: 300;
-  font-size: 1em;
-  text-decoration: none;
-  color: white;
-  transition: all 0.3s ease-in-out;
-  &:hover {
-    // transition: all 0.6s ease-out;
-    // box-shadow: 0px -5px 0 0 orange inset;
-    // color: #a7b2cc;
-    color: white;
-    border-bottom: 10px solid orange;
-  }
-  &.highlight {
-    color: orange !important;
-    font-weight: 500;
-  }
-}
-</style>
