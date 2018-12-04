b92c9c58056bf42a90270dcf678d660b13853ea5
asif <asif.mehedi@live.com>
2018-12-03 19:39:14 -0500

    Test new map component

--- pages/test.vue
+++ pages/test.vue
@@ -1,7 +1,27 @@
+<script>
+// import components
+import MapWithCircles from '@/components/MapWithCircles';
+
+// import data or data-promises
+import { patentByCountryProm } from '@/assets/js/fetchData';
+
+export default {
+  components: { MapWithCircles },
+  data() {
+    return {
+      patentCountByCountry: [],
+    };
+  },
+  mounted() {
+    patentByCountryProm.then(data => {
+      this.patentCountByCountry = data;
+    });
+  },
+};
+</script>
+
 <template lang="pug">
- .uk-container
-  p texting new mixin 
-  .uk-card.bg-orange-50
-    .uk-h3.fg-blue-100
-      p Cupidatat nisi aliqua dolor id eu culpa incididunt non ea eu enim sunt. Laborum aliquip et pariatur veniam. Ut ea consequat culpa ea magna magna velit excepteur veniam magna eiusmod. Occaecat nostrud ea cillum sint ex tempor ea officia. Mollit do amet qui duis.
+.uk-section
+  .uk-container
+    map-with-circles
 </template>
