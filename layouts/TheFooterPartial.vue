6c1d0a3a432de16fe6126aea2b9977e84a0e82df
asif <asif.mehedi@live.com>
2018-12-03 19:37:56 -0500

    Fix footer button

--- layouts/TheFooterPartial.vue
+++ layouts/TheFooterPartial.vue
@@ -1,15 +1,15 @@
 <template lang="pug">
   //- footer
   .uk-section.uk-section-secondary
-    .uk-container.uk-padding
+    .uk-container.uk-container-large
       .uk-grid(uk-grid).uk-child-width-1-2
         .uk-card.uk-card-body.uk-card-secondary.bg-blue-400
-          p <span class="uk-text-uppercase">The University of Virginia Darden School of Business</span> delivers the world’s best business education experience to prepare entrepreneurial, global and responsible leaders through its MBA, Ph.D. and Executive Education programs. Darden’s top-ranked faculty is renowned for teaching excellence and advances practical business knowledge through research. Darden was established in 1955 at the University of Virginia, a top public university founded by Thomas Jefferson in 1819 in Charlottesville, Virginia.
+          p <span class="uk-text-uppercase uk-text-large">The University of Virginia Darden School of Business</span> delivers the world’s best business education experience to prepare entrepreneurial, global and responsible leaders through its MBA, Ph.D. and Executive Education programs. Darden’s top-ranked faculty is renowned for teaching excellence and advances practical business knowledge through research. Darden was established in 1955 at the University of Virginia, a top public university founded by Thomas Jefferson in 1819 in Charlottesville, Virginia.
           .uk-button.uk-button-default
-            a(href="http://www.darden.virginia.edu/") Learn more
+            a(href="http://www.darden.virginia.edu/").uk-link-reset Learn more
         .uk-card.uk-card-body.uk-card-secondary.bg-blue-300
-          p.uk-text-uppercase Contact Information
-            p (TODO: How and who to contact regarding the project)
+          h3 Contact
+          p (TODO: How and who to contact regarding the project)
 
         
     .uk-container.uk-text-center.uk-padding.fg-white
