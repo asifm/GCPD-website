35f1b2a337bb6114f6f8b80f18d71268d1df6a54
asif <asif.mehedi@live.com>
2018-12-10 15:55:40 -0500

    Rename, reorganize, remove components

--- components/FeatureDownloadExtract.vue
+++
@@ -1,1 +0,0 @@
-// todo: add this to all graphs and lists — to allow downloading the filtered data. only gvkey, company names, and number of patents
---
+++ components/outputs/FeatureDownloadDataExtract.vue
@@ -0,0 +1,1 @@
+// todo: add this to all graphs and lists — to allow downloading the filtered data. only gvkey, company names, and number of patents
