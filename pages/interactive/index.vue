75f1a6add5b7e6da907f11969e4dcfeb9800dcc2
asif <asif.mehedi@live.com>
2018-12-11 13:08:39 -0500

    Set spinner function to mounted; better layout for buttons

--- pages/interactive/index.vue
+++ pages/interactive/index.vue
@@ -35,39 +35,38 @@ export default {
       defaultIndustry: 'All Industries',
       startYear: 2000,
       endYear: 2017,
-      listLength: 10,
+      listLength: 30,
       componentToShow: 'both',
-      spinOn: false,
+      spinOn: true,
     };
   },
-  watch: {
-    // $data: {
-    //   handler: function(newVal, oldVal) {
-    //     console.log('handler activated', newVal, oldVal);
-    //     this.spinOn = true;
-    //     setTimeout(() => {
-    //       this.spinOn = false;
-    //     }, 3000);
-    //   },
-    //   deep: true,
-    // },
+  mounted() {
+    // this.spinOn = true;
+    setTimeout(() => {
+      this.spinOn = false;
+    }, 4500);
   },
   beforeUpdate() {
     FilterBus.$on('new-data', () => {
       this.spinOn = true;
       setTimeout(() => {
         this.spinOn = false;
-      }, 3000);
+      }, 2000);
     });
   },
   updated() {
     // console.log('updated');
   },
   methods: {
-    exploreBySingleYear(event) {
-      console.log('change received');
-      this.startYear = Number(event.target.value);
-      this.endYear = this.startYear;
+    // todo: by single year; by single decade
+    // exploreBySingleYear(event) {
+    //   console.log('change received');
+    //   this.startYear = Number(event.target.value);
+    //   this.endYear = this.startYear;
+    // },
+
+    resetData() {
+      FilterBus.$emit('reset-data');
     },
   },
 };
@@ -76,49 +75,56 @@ export default {
 <template lang="pug">
 .uk-section
   .uk-container.uk-container-expand
-    .uk-h2 <span class="my-text-heavy fg-blue"> Explore </span> the World of Corporate Patents
-    .uk-grid.uk-grid-small(uk-grid uk-sortable)
-      .uk-width-3-4(uk-sortable)
-        .uk-width-1-1(uk-grid uk-sortable).uk-grid
-          .uk-width-1-4(uk-sortable)
+    .uk-h1 <span class="my-text-heavy fg-blue"> Explore </span> the World of Corporate Patents
+    div.uk-position-absolute.uk-position-top-center
+      span(uk-spinner="ratio:4" v-show="spinOn").fg-blue-300 Calculating...
+    .uk-grid.uk-grid-small(uk-grid )
+      .uk-width-3-4
+        .uk-width-1-1(uk-grid).uk-grid.uk-grid-small
+          .uk-width-1-4
             select-param-from-list(param-list="countries" :selected_="defaultCountry").uk-margin-small-bottom.bg-orange-50.uk-box-shadow-medium
             select-param-from-list(param-list="industries" :selected_="defaultIndustry").uk-margin-small-bottom.bg-orange-50.uk-box-shadow-medium
 
-          .uk-width-1-4.uk-grid(uk-grid uk-sortable).uk-grid-collapse
-            .uk-width-1-4(uk-sortable)
-              input-number(type="number" event-name="change-startyear" :selected_.sync="startYear").uk-form-width-small.uk-box-shadow-medium
-              input-number(type="number" event-name="change-endyear" :selected_.sync="endYear").uk-form-width-small.uk-box-shadow-medium
-            .uk-width-1-2(uk-sortable)
-              input-number(@change="exploreBySingleYear" type="range" event-name="change-startyear" :selected_.sync="startYear").uk-range.uk-box-shadow-medium
+          .uk-width-1-2.uk-grid(uk-grid )
+            .uk-width-1-4
+              input-number(type="number" event-name="change-startyear" :selected_.sync="startYear").uk-box-shadow-medium.uk-form-width-large
+              input-number(type="number" event-name="change-endyear" :selected_.sync="endYear").bg-blue-50.uk-box-shadow-medium
+            
+            .uk-width-1-2.uk-margin-remove.uk-padding-remove
+              input-number(type="range" event-name="change-startyear" :selected_.sync="startYear").uk-range.uk-box-shadow-medium
               input-number(type="range" event-name="change-endyear" :selected_.sync="endYear").uk-range.bg-blue-50.uk-box-shadow-medium
           
-          .uk-width-1-4(uk-sortable)
-            button.uk-button.uk-button-small.bg-blue-100(@click="componentToShow='map'") Only Map  
-            button.uk-button.uk-button-small.bg-blue-50(@click="componentToShow='graphs'") Only Graphs
-            button.uk-button.uk-button-small.bg-orange-200(@click="componentToShow='both'") Both 
-            
           .uk-width-1-4
-            span(uk-spinner="ratio:3" v-show="spinOn") Calculating...
+            .uk-button-group
+              button.uk-button.uk-button-secondary.uk-button-small(@click="resetData") Reset
+              button.uk-button.uk-button-primary.uk-button-small Tour
+            div
+              div.uk-button-group.uk-margin-top
+                button.uk-button.my-text-tiny.uk-button-text(@click="componentToShow='map'").uk-margin-small-right Only Map
+                button.uk-button.my-text-tiny.uk-button-text(@click="componentToShow='graphs'").uk-margin-small-right Only Graphs
+                button.uk-button.my-text-tiny.uk-button-text(@click="componentToShow='both'") Both
+            
+          
             //- p Explore by Decade
             //- input(@change.number="viewByDecade" type="range" step="10" min="1950" max="2020").uk-form-width-medium.uk-box-shadow-medium
 
                
-        .uk-width-1-1.uk-padding-remove.uk-margin-top.uk-box-shadow-small(uk-sortable)
-          div(v-show="componentToShow === 'map' || componentToShow === 'both'").uk-animation-fade
+        .uk-width-1-1.uk-padding-remove.uk-margin-top.uk-box-shadow-small
+          div(v-show="componentToShow === 'map' || componentToShow === 'both'")
             div
-              map-with-circles(:width="1200" :height="600").uk-animation-fade
+              map-with-circles(:width="1200" :height="600")
           
-          div(v-show="componentToShow === 'graphs' || componentToShow === 'both'" uk-sortable).uk-padding-small.uk-animation-fade
-            .uk-grid(uk-grid uk-sortable).uk-child-width-expand
-              div
-                graph-bar(:chartWidth="550" :chartHeight="350" dataDimension="country" chartType="bar")
-              div
-                graph-bar(:chartWidth="550" :chartHeight="350" dataDimension="industry" chartType="bar")
-            .uk-grid(uk-grid uk-sortable).uk-child-width-expand  
-              div
-                graph-line(:chartWidth="550" :chartHeight="350" dataDimension="year" chartType="area")
-              div
-                graph-donut(:chartWidth="500" :chartHeight="300" dataDimension="region" chartType="donut")
+          //- div(v-show="componentToShow === 'graphs' || componentToShow === 'both'" ).uk-padding-small.uk-animation-fade
+          //-   .uk-grid(uk-grid ).uk-child-width-expand
+          //-     div
+          //-       graph-bar(:chartWidth="550" :chartHeight="350" dataDimension="country" chartType="bar")
+          //-     div
+          //-       graph-bar(:chartWidth="550" :chartHeight="350" dataDimension="industry" chartType="bar")
+          //-   .uk-grid(uk-grid ).uk-child-width-expand  
+          //-     div
+          //-       graph-line(:chartWidth="550" :chartHeight="350" dataDimension="year" chartType="area")
+          //-     div
+          //-       graph-donut(:chartWidth="500" :chartHeight="300" dataDimension="region" chartType="donut")
     
       .uk-width-1-4
         compute-data(:country_="defaultCountry" :industry_="defaultIndustry" :startYear_="startYear" :endYear_="endYear")
