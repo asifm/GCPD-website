ca64fd3881d5864b7a45e61495daa0d235542095
asif <asif.mehedi@live.com>
2018-12-19 16:33:53 -0500

    Add Batten logo, highlight Batten, change layout: footer

--- layouts/TheFooterPartial.vue
+++ layouts/TheFooterPartial.vue
@@ -1,27 +1,34 @@
 <template lang="pug">
-  //- footer
-  .uk-section.uk-section-secondary
-    div
-      div(uk-grid).uk-child-width-expand.uk-grid
-        .uk-tile.fg-white.uk-padding-small
-          .uk-width-3-4.uk-align-right
-            p <span class="uk-h3 uk-text-uppercase uk-text-large">The University of Virginia Darden School of Business</span> delivers the world’s best business education experience to prepare entrepreneurial, global and responsible leaders through its MBA, Ph.D. and Executive Education programs. Darden’s top-ranked faculty is renowned for teaching excellence and advances practical business knowledge through research. Darden was established in 1955 at the University of Virginia, a top public university founded by Thomas Jefferson in 1819 in Charlottesville, Virginia.
+div
+  .uk-section.uk-padding-remove
+    .uk-container.uk-container-expand.uk-padding-remove
+      
+      div(uk-grid).uk-grid.uk-card-secondary.uk-padding-remove
+        .uk-tile.uk-width-1-3
+          img(src="https://res.cloudinary.com/asifm/image/upload/w_250/v1544568270/batten-logo-long_tpsmit.jpg").bg-white.uk-padding.uk-position-medium
+        .uk-tile.uk-padding-large.uk-width-1-3
+          div
+            p This website, including the data tools and visualizations in it, has been developed and is hosted by <span class="uk-text-bold"> The Batten Institute for Entrepreneurship and Innovation </span>at the University of Virginia Darden School. This is part of the long-term support the Institute has been providing in various forms to this project. 
+            p The Institute's mission is to improve society by creating knowledge about the transformative power of entrepreneurship and innovation and by cultivating principled, entrepreneurial leaders.
+            
+            .uk-button-group
+              a.uk-icon-button(uk-icon="icon: mail" href="Batten@darden.virginia.edu")
+              span.uk-margin-small-right Batten@darden.virginia.edu
+          
+              .a.uk-icon-button(uk-icon="icon: twitter" href="http://www.twitter.com/BattenInstitute")
+              span @BattenInstitute
+              
+          .uk-button.uk-button-default.uk-button-small.uk-margin-small-top
+            a(href="https://www.darden.virginia.edu/batten-institute/about/").uk-link-reset Learn more
+            
+        .uk-tile.uk-padding-large.uk-width-1-3
+          div
+            p <span class="uk-text-bold">The University of Virginia Darden School of Business</span> delivers the world’s best business education experience to prepare entrepreneurial, global and responsible leaders through its MBA, Ph.D. and Executive Education programs. Darden’s top-ranked faculty is renowned for teaching excellence and advances practical business knowledge through research. 
+            p Darden was established in 1955 at the University of Virginia, a top public university founded by Thomas Jefferson in 1819 in Charlottesville, Virginia.
               .uk-button.uk-button-default.uk-button-small
                 a(href="http://www.darden.virginia.edu/").uk-link-reset Learn more
-        .uk-tile.fg-white.uk-padding-small
-          .uk-width-3-4.uk-align-left
-            p <span class="uk-h3 uk-text-uppercase uk-text-large">The Batten Institute for Entrepreneurship and Innovation</span> seeks to improve society by creating knowledge about the transformative power of entrepreneurship and innovation and by cultivating principled, entrepreneurial leaders.
-              .uk-button.uk-button-default.uk-button-small
-                a(href="https://www.darden.virginia.edu/batten-institute/about/").uk-link-reset Learn more
             
-            p.uk-margin-large-top
-              a.uk-icon-button.uk-margin-small-right(uk-icon="icon: mail" href="Batten@darden.virginia.edu").uk-button-primary
-              span Batten@darden.virginia.edu
-            p
-              a.uk-icon-button.uk-margin-small-right(uk-icon="icon: twitter" href="http://www.twitter.com/BattenInstitute").uk-button-primary 
-              span @BattenInstitute
-            
-    .uk-divider-icon
+    
     .uk-text-center.uk-padding-small.bg-blue.fg-white
       p COPYRIGHT © 2018 THE RECTOR AND VISITORS OF THE UNIVERSITY OF VIRGINIA. ALL RIGHTS RESERVED.
 </template>
