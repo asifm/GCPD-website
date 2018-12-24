aee4b7d09ce3b8b29d0034e07fdf2f288560c1fd
asif <asif.mehedi@live.com>
2018-12-23 20:46:05 -0500

    Update formatNumber function; better naming: format -> d3Format

--- assets/js/utility.js
+++ assets/js/utility.js
@@ -1,24 +1,39 @@
-import { format } from 'd3-format';
+// @ts-nocheck
+
+import { format as d3Format } from 'd3-format';
 
 // Needed for apexcharts year axis
 export function fillRange(start, end) {
-  return Array(end - start + 1)
-    .fill()
-    .map((item, index) => start + index);
+  return (
+    Array(end - start + 1)
+      // @ts-ignore
+      .fill()
+      .map((_, index) => start + index)
+  );
 }
 // format numbers conditionally; mainly for apexcharts
-export function formatVal(val, opt) {
-  val = +val;
+export function formatNumber(
+  val,
+  valMultiplier = 1,
+  numDigitsAfterDecimal = 2,
+) {
+  val = +val * valMultiplier;
   let formattedVal;
   switch (true) {
-    case val >= 1000000:
-      formattedVal = format(',')((val / 1000000).toFixed(2)) + 'M';
+    case val >= 1e9:
+      formattedVal =
+        d3Format(',')((val / 1e9).toFixed(numDigitsAfterDecimal)) + ' B';
       break;
-    case val >= 10000:
-      formattedVal = format(',')(Math.round(val / 1000)) + 'K';
+    case val >= 1e6:
+      formattedVal =
+        d3Format(',')((val / 1e6).toFixed(numDigitsAfterDecimal)) + ' M';
       break;
-    case val < 10000:
-      formattedVal = format(',')(val);
+    case val >= 1e4:
+      formattedVal =
+        d3Format(',')((val / 1e3).toFixed(numDigitsAfterDecimal)) + ' K';
+      break;
+    case val < 1e4:
+      formattedVal = d3Format(',')(val);
       break;
   }
   return formattedVal;
