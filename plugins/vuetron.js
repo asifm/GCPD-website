aaf506a14ffa408569af7f7a351a4c249b16ca0a
asif <asif.mehedi@live.com>
2018-12-23 21:10:06 -0500

    Remove config of port 3000

--- plugins/vuetron.js
+++
@@ -1,6 +0,0 @@
-// debugging tool: https://github.com/vuetwo/vuetron
-
-// important: Turned off for now; to stop socket.io polling errors.
-import Vue from 'vue';
-import vuetron from 'vuetron';
-Vue.use(vuetron.VuetronVue);
