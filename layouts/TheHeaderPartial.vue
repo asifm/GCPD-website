8e3ecbd52420deeb8dd7909d9f1d44b892cf4e04
asif <asif.mehedi@live.com>
2018-12-28 20:14:21 -0500

    Update TheHeaderPartial.vue
    Remove submenus in nav

--- layouts/TheHeaderPartial.vue
+++ layouts/TheHeaderPartial.vue
@@ -1,5 +1,5 @@
 <template lang="pug">
-  nav.uk-navbar-right(uk-navbar uk-sticky="show-on-up:true; animation:uk-animation-slide-top").uk-box-shadow-small.uk-background-muted
+  nav.uk-navbar-right(uk-navbar uk-sticky="show-on-up:true; animation:uk-animation-slide-top").uk-box-shadow-small.bg-blue-50
     img(src="https://res.cloudinary.com/asifm/image/upload/w_300/v1543573588/darden_rgb-02_qrpxwe.png").uk-float-left.uk-margin-left
     .uk-navbar-right.uk-margin-right
       ul.uk-navbar-nav
@@ -13,10 +13,12 @@
           //-     li.uk-button-text: nuxt-link(to="#") Countries
         li.uk-button-text
           nuxt-link(to="/interactive") Interactive
-          .uk-navbar-dropdown
-            ul.uk-nav.uk-navbar-dropdown-nav
-              li.uk-button-text: nuxt-link(to="/interactive/map") The World
-              li.uk-button-text: nuxt-link(to="/interactive/graphs") Companies
+          //- .uk-navbar-dropdown
+            //- ul.uk-nav.uk-navbar-dropdown-nav
+              //- li.uk-button-text: nuxt-link(to="/interactive/explore_all") Explore All Data
+              //- li.uk-button-text: nuxt-link(to="/interactive/companies") Companies
+              //- li.uk-button-text: nuxt-link(to="/interactive/industries") Industries
+              //- li.uk-button-text: nuxt-link(to="/interactive/countries") Countries
 
         li.uk-button-text: nuxt-link(to="/publications") Publications
         li.uk-button-text: nuxt-link(to="/download_data") Download Data
