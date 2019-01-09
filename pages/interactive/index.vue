dc49d652a05ae1d827681bd7366aaf3d621f2603
asif <asif.mehedi@live.com>
2019-01-09 15:54:02 -0500

    Remove apexcharts

--- pages/interactive/index.vue
+++ pages/interactive/index.vue
@@ -1,61 +1,65 @@
 <script>
-// ::import npm modules
-// ::import components
-import { FilterBus } from '@/assets/js/FilterBus';
+// todo: change layout of controls: one below another instead of side by side
+// todo: create cleaner layout; use flex?
+import introJs from 'intro.js';
 
-import SelectParamFromList from '@/components/inputs/SelectParamFromList';
-import InputNumber from '@/components/inputs/InputNumber';
+import { lists } from '@/assets/data/listData';
+import { FilterBus } from '@/assets/js/FilterBus';
 
 import ComputeData from '@/components/computes/ComputeData';
 
+import SelectParameter from '@/components/inputs/SelectParameter';
+import InputRange from '@/components/inputs/InputRange';
+
 import ListCompanyDetailed from '@/components/outputs/ListCompanyDetailed';
 import MapWithCircles from '@/components/outputs/MapWithCircles';
-import GraphBar from '@/components/outputs/GraphBar';
-import GraphLine from '@/components/outputs/GraphLine';
-import GraphDonut from '@/components/outputs/GraphDonut';
-import { setTimeout } from 'timers';
 
-// todo p2: get country name by clicking on the map
 // todo p1: Remove from data the unimportant countries
 
 export default {
   components: {
-    SelectParamFromList,
-    InputNumber,
+    SelectParameter,
+    InputRange,
     ListCompanyDetailed,
     MapWithCircles,
     ComputeData,
-    GraphBar,
-    GraphLine,
-    GraphDonut,
   },
   data() {
     return {
       defaultCountry: 'All Countries',
       defaultIndustry: 'All Industries',
-      startYear: 2000,
-      endYear: 2017,
-      listLength: 30,
+      rangeYears: [lists.dataYearRange.min, lists.dataYearRange.max],
+      minYear: lists.dataYearRange.min,
+      maxYear: lists.dataYearRange.max,
+      listLength: 20,
       componentToShow: 'both',
-      spinOn: true,
     };
   },
+  computed: {
+    startYear: {
+      get() {
+        return this.rangeYears[0];
+      },
+      set(newValue) {
+        this.rangeYears[0] = newValue;
+      },
+    },
+    endYear: {
+      get() {
+        return this.rangeYears[1];
+      },
+      set(newValue) {
+        this.rangeYears[1] = newValue;
+      },
+    },
+  },
   mounted() {
-    // this.spinOn = true;
-    setTimeout(() => {
-      this.spinOn = false;
-    }, 4500);
-  },
-  beforeUpdate() {
-    FilterBus.$on('new-data', () => {
-      this.spinOn = true;
-      setTimeout(() => {
-        this.spinOn = false;
-      }, 2000);
+    // todo: uncomment startTour when ready
+    // this.startTour();
+    FilterBus.$on('new-data', dataObj => {
+      const { rangeYears } = dataObj;
+      this.rangeYears = rangeYears;
     });
-  },
-  updated() {
-    // console.log('updated');
   },
   methods: {
     // todo: by single year; by single decade
@@ -66,68 +70,123 @@ export default {
     // },
 
     resetData() {
+      // todo: Use $on to reset all data in other components
       FilterBus.$emit('reset-data');
     },
+    startTour() {
+      // todo: use json to define steps (https://github.com/usablica/intro.js/blob/master/example/programmatic/index.html)
+      introJs().start();
+    },
+    addHints() {
+      // todo: add buttons for hints
+      introJs().addHints();
+    },
+  },
+  head() {
+    return {
+      link: [
+        {
+          rel: 'stylesheet',
+          href:
+            'https://cdnjs.cloudflare.com/ajax/libs/intro.js/2.9.3/introjs.min.css',
+        },
+      ],
+    };
   },
 };
 </script>
 
 <template lang="pug">
-.uk-section
-  .uk-container.uk-container-expand
-    .uk-h1 <span class="my-text-heavy fg-blue"> Explore </span> the World of Corporate Patents
-    div.uk-position-absolute.uk-position-top-center
-      span(uk-spinner="ratio:4" v-show="spinOn").fg-blue-300 Calculating...
-    .uk-grid.uk-grid-small(uk-grid )
-      .uk-width-3-4
-        .uk-width-1-1(uk-grid).uk-grid.uk-grid-small
-          .uk-width-1-4
-            select-param-from-list(param-list="countries" :selected_="defaultCountry").uk-margin-small-bottom.bg-orange-50.uk-box-shadow-medium
-            select-param-from-list(param-list="industries" :selected_="defaultIndustry").uk-margin-small-bottom.bg-orange-50.uk-box-shadow-medium
+div.uk-section
+  div.uk-container.uk-container-expand
+    div.uk-h2(
+      data-intro="Use this tool to explore the entire dataset."
+      ) <span class="my-text-heavy fg-blue-200"> Explore </span> the World of Corporate Patents
+    div.uk-grid.uk-grid-small(uk-grid)
+      div.uk-width-3-4
+        div.uk-width-1-1.uk-grid.uk-grid-small(uk-grid)
+          div.uk-width-1-4(
+            data-step=2
+            data-intro="Select a country or an industry. <br><br> All the data and visualizations will change to reflect your selection."
+            )
+            select-parameter.uk-margin-small-bottom.bg-orange-50.uk-box-shadow-medium(
+              :selected_="defaultCountry"
+              param-list="countries" 
+              )
+            select-parameter.uk-margin-small-bottom.bg-orange-50.uk-box-shadow-medium(
+              :selected_="defaultIndustry"
+              param-list="industries" 
+              )
 
-          .uk-width-1-2.uk-grid(uk-grid )
-            .uk-width-1-4
-              input-number(type="number" event-name="change-startyear" :selected_.sync="startYear").uk-box-shadow-medium.uk-form-width-large
-              input-number(type="number" event-name="change-endyear" :selected_.sync="endYear").bg-blue-50.uk-box-shadow-medium
-            
-            .uk-width-1-2.uk-margin-remove.uk-padding-remove
-              input-number(type="range" event-name="change-startyear" :selected_.sync="startYear").uk-range.uk-box-shadow-medium
-              input-number(type="range" event-name="change-endyear" :selected_.sync="endYear").uk-range.bg-blue-50.uk-box-shadow-medium
-          
-          .uk-width-1-4
-            .uk-button-group
-              button.uk-button.uk-button-secondary.uk-button-small(@click="resetData") Reset
-              button.uk-button.uk-button-primary.uk-button-small Tour
+          div.uk-width-1-2.uk-grid(
+            uk-grid
+            data-step=3 
+            data-intro="Filter the data by years. Change either year or both. <br><br> If the first year matches the last year, you'll get the data for that single year. <br><br>You can also move these sliders around to change the time period."
+            )
+            //- todo: remove these; use them as read-only labels instead for the slider
+            div.uk-width-1-4
+              input.uk-input(
+                v-model="startYear"
+                :min="minYear" 
+                :max="maxYear" 
+                type="number" 
+                readonly
+                )
+              input.uk-input(
+                v-model="endYear"
+                :min="minYear" 
+                :max="maxYear" 
+                type="number" 
+                readonly
+                )
+            div.uk-width-1-2
+              input-range.el-form-item--medium(
+                :rangeYears_="rangeYears"
+                )
+          div.uk-width-1-4
+            div.uk-button-group(
+              data-step=7 
+              data-intro="At any point, you can set everything on the page to its beginning state. <br><br> You can also start this tour from here. <br><br> The smaller buttons let you customize the view to your preference."
+              )
+              button.uk-button.uk-button-secondary.uk-button-small(
+                @click="resetData"
+                ) Reset
+              button.uk-button.uk-button-primary.uk-button-small(
+                @click="startTour"
+                ) Tour
+              button.uk-button.uk-button-success.uk-button-small(
+                @click="addHints"
+                ) Hints
             div
               div.uk-button-group.uk-margin-top
-                button.uk-button.my-text-tiny.uk-button-text(@click="componentToShow='map'").uk-margin-small-right Only Map
-                button.uk-button.my-text-tiny.uk-button-text(@click="componentToShow='graphs'").uk-margin-small-right Only Graphs
-                button.uk-button.my-text-tiny.uk-button-text(@click="componentToShow='both'") Both
-            
-          
-            //- p Explore by Decade
-            //- input(@change.number="viewByDecade" type="range" step="10" min="1950" max="2020").uk-form-width-medium.uk-box-shadow-medium
-
-               
-        .uk-width-1-1.uk-padding-remove.uk-margin-top.uk-box-shadow-small
+                button.uk-button.my-text-tiny.uk-button-text.uk-margin-small-right(
+                  @click="componentToShow='map'"
+                  ) Only Map
+                button.uk-button.my-text-tiny.uk-button-text.uk-margin-small-right(
+                  @click="componentToShow='graphs'"
+                  ) Only Graphs
+                button.uk-button.my-text-tiny.uk-button-text(
+                  @click="componentToShow='both'"
+                  ) Both
+        div.uk-width-1-1.uk-padding-remove.uk-margin-top.uk-box-shadow-small 
           div(v-show="componentToShow === 'map' || componentToShow === 'both'")
-            div
-              map-with-circles(:width="1200" :height="600")
+            div(
+              data-step=4 
+              data-intro="The circles on the map show total patent counts by country. To see the data for a country, hover over its circle. <br><br> Double click to zoom and drag to pan. Get back to full view using the reset button above.<br><br> When the data changes, the circles rescale based on the then current minimum and maximum."
+              )
+              map-with-circles(
+                :width="1200" 
+                :height="600"
+                )
           
-          //- div(v-show="componentToShow === 'graphs' || componentToShow === 'both'" ).uk-padding-small.uk-animation-fade
-          //-   .uk-grid(uk-grid ).uk-child-width-expand
-          //-     div
-          //-       graph-bar(:chartWidth="550" :chartHeight="350" dataDimension="country" chartType="bar")
-          //-     div
-          //-       graph-bar(:chartWidth="550" :chartHeight="350" dataDimension="industry" chartType="bar")
-          //-   .uk-grid(uk-grid ).uk-child-width-expand  
-          //-     div
-          //-       graph-line(:chartWidth="550" :chartHeight="350" dataDimension="year" chartType="area")
-          //-     div
-          //-       graph-donut(:chartWidth="500" :chartHeight="300" dataDimension="region" chartType="donut")
-    
-      .uk-width-1-4
-        compute-data(:country_="defaultCountry" :industry_="defaultIndustry" :startYear_="startYear" :endYear_="endYear")
-        list-company-detailed(:listLength="listLength").uk-box-shadow-large
+      div.uk-width-1-4
+        compute-data
+        list-company-detailed.uk-box-shadow-large(
+          :listLength="listLength" 
+          data-position="auto" 
+          data-scrollTo="#top" 
+          data-step=6 
+          data-intro="List of the top companies. Move the slider to adjust the list size. <br><br>The background color indicates the region of a company's headquarters. <br><br> See detailed data about a company by hovering over its name."
+          )
         
 </template>
