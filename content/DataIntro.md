5296a67585f752145ffd1c4296cfabb8794fb30e
asif <asif.mehedi@live.com>
2019-04-18 12:24:20 -0400

    Edit text content

--- content/DataIntro.md
+++ content/DataIntro.md
@@ -2,10 +2,8 @@
 
 The dataset that we have made available on this website provides a window into the world of corporate patents—an important indicator of corporate research and development activities and the consequent innovation—across industries and countries.
 
-The public release of this dataset reflects our belief that it would open up new research possibilities and enable the discovery of insights in different areas of finance and economics. We also expect this data to help advance public discussions, outside of academia, on corporate innovation, productivity, and performance.
+We believe the public release of this dataset, of patents awarded to publicly listed companies, would open up new research possibilities and enable the discovery of insights in different areas of finance and economics. We also expect this data to help advance public discussions, outside of academia, on corporate innovation, productivity, and performance.
 
 <hr class="uk-icon-divider">
 
-This research and data project was made possible with the financial support from the <em>Batten Institute for Entrepreneurship and Innovation</em> and the <em>Richard A. Mayo Center for Asset Management</em> at the UVA Darden School of Business.
-
-
+This research project was made possible with the financial support from the <em>Batten Institute for Entrepreneurship and Innovation</em> and the <em>Richard A. Mayo Center for Asset Management</em> at the UVA Darden School of Business.
--- pages/data_highlights/index.vue
+++ pages/data_highlights/index.vue
@@ -5,7 +5,7 @@
     h1 Top Five Companies by Patent Count (2001–2017)
     div
       img(src="https://res.cloudinary.com/asifm/image/upload/w_400/v1544462450/region_change_qdxekx.png").uk-float-right
-      p The firms in our dataset were granted close to 2 million U.S. patents from 2001 to 2017. The patents were well distributed across countries, illustrating the global nature of innovation.
+      p In our dataset, the firms, all publicly listed, were granted close to 2 million U.S. patents from 2001 to 2017. The patents were well distributed across countries, illustrating the global nature of innovation.
 
       p Non-U.S. firms filed more than half of the USPTO patents in this period. Surpassing U.S. firms, Japanese companies had the highest number of patents per firm. Overall, European firms filed fewer patents than North American or Asian firms, although German firms produced significant innovation.
 
@@ -35,17 +35,33 @@
             img(src="https://res.cloudinary.com/asifm/image/upload/w_800/v1542502401/regionshare-bar-1950-2017_fsv2o3.png")
         
         .uk-width-1-2
-          .uk-card.uk-card-body.uk-card-default(uk-scrollspy="cls:uk-animation-slide-top-small; delay: 100; repeat: true")
-            .uk-h4.uk-card-title Technology industry leading in innovation
-            p Consistent with broader trends in business and society, innovation now more often happens in the technology-focused companies — companies with a significant technological component in their business operations and products —than in any other industry. At the time the technology industry was gaining prominence, the manufacturing industry was losing ground. This shift is not surprising since, during these decades, manufacturing businesses were increasingly taking place in the developing world.
-            img(src="https://res.cloudinary.com/asifm/image/upload/v1542499302/industryshare-bar-1950-2017_ymsqbq.png")
+          //- .uk-card.uk-card-body.uk-card-default(uk-scrollspy="cls:uk-animation-slide-top-small; delay: 100; repeat: true")
+          .uk-h1 Technology industry leading in innovation
+          p Consistent with broader trends in business and society, innovation now more often happens in the technology-focused companies — companies with a significant technological component in their business operations and products —than in any other industry. At the time the technology industry was gaining prominence, the manufacturing industry was losing ground. 
+          img(src="https://res.cloudinary.com/asifm/image/upload/v1542499302/industryshare-bar-1950-2017_ymsqbq.png")
+        
+          //- .uk-card.uk-card-body.uk-card-default(uk-scrollspy="cls:uk-animation-slide-top-small; delay: 100; repeat: true")
+          p While this shift from manufacturing to technology is ubiquitous across the three regions, the other industries present a mixed picture. For example, innovation is particularly strong in high-tech in Asia. But European companies innovate more than their Asian counterparts when it comes to healthcare and chemicals.
+          img(src="https://res.cloudinary.com/asifm/image/upload/v1542499301/industry-by-region-current_upgcu6.png")
+         
+          p Taking a snapshot of the entire dataset, covering the period 1950–2016, the graph below summarizes the varying magnitude of innovation, as measured by USPTO patents, in twelve different industries.
+          img(src="https://res.cloudinary.com/asifm/image/upload/v1542504117/industryshare-current_k0cugj.png")
+
           
-            
-          .uk-card.uk-card-body.uk-card-default(uk-scrollspy="cls:uk-animation-slide-top-small; delay: 100; repeat: true")
-            p While this shift from manufacturing to technology is ubiquitous across the three regions,  the other industries present a mixed picture. For example, innovation is particularly strong in the wholesale and retail industry of Asia. European companies innovate more than their Asian counterparts when it comes to medical equipment, healthcare, chemicals, and mineral businesses.
-            img(src="https://res.cloudinary.com/asifm/image/upload/v1542499301/industry-by-region-current_upgcu6.png")
-          .uk-card.uk-card-body.uk-card-default(uk-scrollspy="cls:uk-animation-slide-top-small; delay: 100; repeat: true")
-            p Taking a snapshot of the entire dataset as it stands now, the graph below summarizes the varying contribution of the twelve different industries to innovation as measured by USPTO patents.
-            img(src="https://res.cloudinary.com/asifm/image/upload/v1542504117/industryshare-current_k0cugj.png")
+          .uk-h5.uk-padding-top Note on industry classification
+          p Firms are grouped according to the Fama-French 12 industry groups:
+          ul.uk-list.uk-list-bullet
+            li Consumer NonDurables: Food, Tobacco, Textiles, Apparel, Leather, Toys
+            li Consumer Durables: Cars, TV's, Furniture, Household Appliances
+            li Manufacturing: Machinery, Trucks, Planes, Chemicals, Office Furniture, Paper, Com Printing
+            li Energy: Oil, Gas, and Coal Extraction and Products
+            li High Tech (originally named "Business Equipment"): Computers, Software, and Electronic Equipment
+            li Telecom: Telephone and Television Transmission
+            li Shops: Wholesale, Retail, and Some Services (Laundries, Repair Shops)
+            li Healthcare: Healthcare, Medical Equipment, and Drugs
+            li Utilities
+            li Other: Mines, Constr, BldMt, Trans, Hotels, Bus Serv, Entertainment, Finance
+
+
 
 </template>
