901ecc0b418962fd663052bd083f7e8dae2ec71d
asif <asif.mehedi@live.com>
2019-05-05 06:11:57 -0400

    Add new highcharts charts; remove unnecessary chart files

---
+++ components/outputs/ChartBubble.vue
@@ -0,0 +1,48 @@
+<script>
+import { Chart } from 'highcharts-vue';
+
+import Highcharts from 'highcharts';
+import exportingInit from 'highcharts/modules/exporting';
+import moreInit from 'highcharts/highcharts-more';
+
+exportingInit(Highcharts);
+moreInit(Highcharts);
+
+export default {
+  components: {
+    ChartHighchart: Chart,
+  },
+  props: { customOptions: Object },
+  data() {
+    return {
+      chartOptions: {
+        chart: {
+          type: 'bubble',
+          zoomType: 'xy',
+        },
+        title: {
+          text: '',
+        },
+
+        xAxis: {
+          gridLineWidth: 1,
+        },
+        yAxis: {
+          startOnTick: false,
+          endOnTick: false,
+        },
+        series: [],
+      },
+    };
+  },
+  mounted() {
+    Object.assign(this.chartOptions, this.customOptions);
+  },
+};
+</script>
+
+<template lang="pug">
+div
+  //- p {{ chartOptions }}
+  chart-highchart(:options="chartOptions")
+</template>
---
+++ components/outputs/ChartStacked.vue
@@ -0,0 +1,58 @@
+<script>
+import { Chart } from 'highcharts-vue';
+
+import Highcharts from 'highcharts';
+import streamgraphInit from 'highcharts/modules/streamgraph';
+import seriesLabelInit from 'highcharts/modules/series-label';
+import exportingInit from 'highcharts/modules/exporting';
+
+streamgraphInit(Highcharts);
+seriesLabelInit(Highcharts);
+exportingInit(Highcharts);
+
+export default {
+  components: {
+    ChartHighchart: Chart,
+  },
+  props: { customOptions: Object },
+  data() {
+    return {
+      chartOptions: {
+        plotOptions: {
+          series: {
+            label: {
+              minFontSize: 9,
+              maxFontSize: 15,
+              onArea: true,
+            },
+            // stacking: 'percent',
+          },
+        },
+        chart: {
+          type: 'column',
+          zoomType: 'xy',
+        },
+        series: [],
+        xAxis: {
+          categories: Array.from(Array(37).keys()).map(el => el + 1980),
+          labels: {
+            step: 3,
+          },
+        },
+        yAxis: {
+          visible: true,
+        },
+      },
+    };
+  },
+  mounted() {
+    Object.assign(this.chartOptions, this.customOptions);
+  },
+};
+</script>
+
+<template lang="pug">
+div
+  //- p {{ chartOptions }}
+  chart-highchart(:options="chartOptions")
+</template>
