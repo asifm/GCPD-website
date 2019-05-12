8132c80ac33d6026ba822a1e5bdd1aa12d58c882
asif <asif.mehedi@live.com>
2019-05-11 23:24:22 -0400

    Create get-data/index.vue

---
+++ pages/get-data/index.vue
@@ -0,0 +1,16 @@
+<script>
+import DownloadIntro from '@/content/DownloadIntro.md';
+import DataCitation from '@/content/DataCitation.md';
+
+export default {
+  components: { DownloadIntro, DataCitation },
+};
+</script>
+
+<template lang="pug">
+.uk-section.uk-animation-slide-top-small
+  .uk-container.uk-container-small
+    .uk-card.uk-card-default.uk-card-body.uk-box-shadow-small
+      download-intro
+      <iframe src="https://virginia.az1.qualtrics.com/jfe/form/SV_9B2ivuARIUgDgC9" height="1000px" title="Short form before download"></iframe>
+</template>
