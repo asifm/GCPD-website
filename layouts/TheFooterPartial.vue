be0326d179bed752315c756f9888dd0e533de66a
asif <asif.mehedi@live.com>
2019-05-22 02:13:10 -0400

    Change responsive thresholds

--- layouts/TheFooterPartial.vue
+++ layouts/TheFooterPartial.vue
@@ -7,10 +7,10 @@ footer
   .uk-section-secondary.uk-padding-small.uk-padding-remove-vertical.bg-blue-400.fg-white
     .uk-container.uk-container-expand
       div(uk-grid).uk-grid.uk-padding-remove
-        div(class="uk-width-1-5@s uk-visible@s")
+        div(class="uk-width-1-5@m uk-visible@m")
           img#battenlogo(alt="Batten Institute for Entrepreneurship & Innovation" src="https://res.cloudinary.com/asifm/image/upload/w_500/v1544568270/batten-logo-long_tpsmit.jpg").bg-white.uk-padding-small.uk-position-medium
     
-        div(class="uk-tile uk-padding uk-width-2-5@s")
+        div(class="uk-tile uk-padding uk-width-2-5@m")
           div
             p This website, including the data tools and visualizations in it, has been developed by <span class="uk-text-bold"> the Batten Institute for Entrepreneurship and Innovation </span>at the University of Virginia Darden School of Business. This is part of the long-term support the institute has been providing in various forms to the Global Corporate Patent Dataset project. 
             p The institute's mission is to improve society by creating knowledge about the transformative power of entrepreneurship and innovation and by cultivating principled, entrepreneurial leaders.
@@ -24,7 +24,7 @@ footer
           .uk-button.uk-button-default.uk-button-small.uk-margin-small-top
             a(href="https://www.darden.virginia.edu/batten-institute/about/" target="_blank" rel="noreferrer").uk-link-reset More about Batten Institute
             
-        div(class="uk-tile uk-padding uk-width-2-5@s")
+        div(class="uk-tile uk-padding uk-width-2-5@m")
           div
             p <span class="uk-text-bold">The University of Virginia Darden School of Business</span> delivers the world’s best business education experience to prepare entrepreneurial, global and responsible leaders through its MBA, Ph.D. and Executive Education programs. Darden’s top-ranked faculty is renowned for teaching excellence and advances practical business knowledge through research. 
             p Darden was established in 1955 at the University of Virginia, a top public university founded by Thomas Jefferson in 1819 in Charlottesville, Virginia.
